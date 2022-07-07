bc91d85f78432c948f8d1fc1e97b9c8b6be43a51
awasur04 <awasur04@yahoo.com>
2022-07-07 16:27:23 +0200

    created log in form

---
+++ FlightPlanning/LoginForm.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace FlightPlanning.UI
+{
+    partial class LoginForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LoginForm));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.emailTextBox = new System.Windows.Forms.TextBox();
+            this.passwordTextBox = new System.Windows.Forms.TextBox();
+            this.emailLabel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.loginButton = new System.Windows.Forms.Button();
+            this.registerButton = new System.Windows.Forms.Button();
+            this.showPasswordImage = new System.Windows.Forms.PictureBox();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.showPasswordImage)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(97, 55);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(141, 135);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // emailTextBox
+            // 
+            this.emailTextBox.Location = new System.Drawing.Point(67, 258);
+            this.emailTextBox.Name = "emailTextBox";
+            this.emailTextBox.Size = new System.Drawing.Size(204, 20);
+            this.emailTextBox.TabIndex = 1;
+            // 
+            // passwordTextBox
+            // 
+            this.passwordTextBox.Location = new System.Drawing.Point(67, 302);
+            this.passwordTextBox.Name = "passwordTextBox";
+            this.passwordTextBox.Size = new System.Drawing.Size(204, 20);
+            this.passwordTextBox.TabIndex = 2;
+            this.passwordTextBox.UseSystemPasswordChar = true;
+            // 
+            // emailLabel
+            // 
+            this.emailLabel.AutoSize = true;
+            this.emailLabel.Location = new System.Drawing.Point(64, 242);
+            this.emailLabel.Name = "emailLabel";
+            this.emailLabel.Size = new System.Drawing.Size(35, 13);
+            this.emailLabel.TabIndex = 3;
+            this.emailLabel.Text = "Email:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(64, 286);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Password:";
+            // 
+            // loginButton
+            // 
+            this.loginButton.Location = new System.Drawing.Point(67, 340);
+            this.loginButton.Name = "loginButton";
+            this.loginButton.Size = new System.Drawing.Size(104, 23);
+            this.loginButton.TabIndex = 5;
+            this.loginButton.Text = "Login";
+            this.loginButton.UseVisualStyleBackColor = true;
+            this.loginButton.Click += new System.EventHandler(this.loginButton_Click);
+            // 
+            // registerButton
+            // 
+            this.registerButton.Location = new System.Drawing.Point(172, 340);
+            this.registerButton.Name = "registerButton";
+            this.registerButton.Size = new System.Drawing.Size(104, 23);
+            this.registerButton.TabIndex = 6;
+            this.registerButton.Text = "Register";
+            this.registerButton.UseVisualStyleBackColor = true;
+            this.registerButton.Click += new System.EventHandler(this.registerButton_Click);
+            // 
+            // showPasswordImage
+            // 
+            this.showPasswordImage.BackColor = System.Drawing.SystemColors.Control;
+            this.showPasswordImage.Image = ((System.Drawing.Image)(resources.GetObject("showPasswordImage.Image")));
+            this.showPasswordImage.Location = new System.Drawing.Point(275, 302);
+            this.showPasswordImage.Name = "showPasswordImage";
+            this.showPasswordImage.Size = new System.Drawing.Size(19, 20);
+            this.showPasswordImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.showPasswordImage.TabIndex = 7;
+            this.showPasswordImage.TabStop = false;
+            this.toolTip1.SetToolTip(this.showPasswordImage, "Show/Hide Password");
+            this.showPasswordImage.Click += new System.EventHandler(this.showPasswordImage_Click);
+            // 
+            // LoginForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(342, 490);
+            this.Controls.Add(this.showPasswordImage);
+            this.Controls.Add(this.registerButton);
+            this.Controls.Add(this.loginButton);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.emailLabel);
+            this.Controls.Add(this.passwordTextBox);
+            this.Controls.Add(this.emailTextBox);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "LoginForm";
+            this.Text = "Airline Planner";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.showPasswordImage)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox emailTextBox;
+        private System.Windows.Forms.TextBox passwordTextBox;
+        private System.Windows.Forms.Label emailLabel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button loginButton;
+        private System.Windows.Forms.Button registerButton;
+        private System.Windows.Forms.PictureBox showPasswordImage;
+        private System.Windows.Forms.ToolTip toolTip1;
+    }
+}
+
