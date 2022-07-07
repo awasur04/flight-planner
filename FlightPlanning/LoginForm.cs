885291002197162e4283265ed749ca3141d4a0c1
awasur04 <awasur04@yahoo.com>
2022-07-07 16:31:42 +0200

    added visible password functionality
    Clicking eye icon will now display the password or hide it

--- FlightPlanning/LoginForm.cs
+++ FlightPlanning/LoginForm.cs
@@ -1,3 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using FlightPlanning.Logic;
+using FlightPlanning.Models;
+
+
+namespace FlightPlanning.UI
+{
+    public partial class LoginForm : Form
+    {
+        private bool showPassword = false;
+        private Validate validate;
+        private Database db;
+        
+        public LoginForm()
+        {
+            InitializeComponent();
+            validate = new Validate();
+            db = new Database();
+        }
+
+        #region Button Event Handlers
+        private void registerButton_Click(object sender, EventArgs e)
+        {
+            RegisterForm registerForm = new RegisterForm();
+            registerForm.ShowDialog();
+        }
+
+        private void showPasswordImage_Click(object sender, EventArgs e)
+        {
+            showPassword = !showPassword;
+
+            if (showPassword)
+            {
+                passwordTextBox.UseSystemPasswordChar = false;
+                showPasswordImage.Image = Image.FromFile(Application.StartupPath + "\\resources\\images\\password-show.png");
+            }
+            else
+            {
+                passwordTextBox.UseSystemPasswordChar = true;
+                showPasswordImage.Image = Image.FromFile(Application.StartupPath + "\\resources\\images\\password-hide.png");
+            }
+        }
+        #endregion
+
         private void loginButton_Click(object sender, EventArgs e)
         {
             if(!validate.Email(emailTextBox.Text.ToString().Trim()))
@@ -19,3 +71,6 @@
             }
 
             
+        }
+    }
+}
