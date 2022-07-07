540adde57f784706ed8eaf5fce8e967c64a5854e
awasur04 <awasur04@yahoo.com>
2022-07-07 16:31:55 +0200

    enum to return account creation status

---
+++ FlightPlanning.Models/Enums/CreationStatus.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlightPlanning.Models
+{
+    public enum CreationStatus
+    {
+        SUCCESS,
+        FAILURE,
+        ACCT_ALREADY_EXISTS
+    }
+}
