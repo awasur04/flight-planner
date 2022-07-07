2a0ab921f83cb9e5703a6d1401c6f0be6b1de08b
awasur04 <awasur04@yahoo.com>
2022-07-07 16:32:34 +0200

    created user model
    User model to store email, name, id, and password

---
+++ FlightPlanning.Models/Classes/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlightPlanning.Models
+{
+    public class User
+    {
+        public string email { get; set; }
+        public string password { get; set; }
+        public string name { get; set; }
+        public int id { get; set; }
+
+        public User(string email, string password, string name, int id)
+        {
+            this.email = email;
+            this.password = password;
+            this.name = name;
+            this.id = id;
+        }
+    }
+}
