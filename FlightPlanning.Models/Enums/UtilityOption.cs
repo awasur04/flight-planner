d14bec1a94717d8fdf7c5cef66ebd3c0f9d18651
awasur04 <awasur04@yahoo.com>
2022-07-08 14:26:40 +0200

    added enum for account utility option
    enum to represent account utility option selected for changing the active screen on Account management window

---
+++ FlightPlanning.Models/Enums/UtilityOption.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlightPlanning.Models
+{
+    public enum UtilityOption
+    {
+        CHANGE_EMAIL,
+        CHANGE_PASSWORD,
+        CLOSE_ACCOUNT
+    }
+}
