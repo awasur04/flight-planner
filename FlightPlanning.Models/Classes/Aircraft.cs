f0c240033853a172fe364eb9a3c57287b776a2e3
awasur04 <awasur04@yahoo.com>
2022-07-13 17:44:06 +0200

    implemented model to represent aircrafts

---
+++ FlightPlanning.Models/Classes/Aircraft.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlightPlanning.Models
+{
+    /*TODO: https://rapidapi.com/aedbx-aedbx/api/aerodatabox/pricing
+
+    */
+    public class Aircraft
+    {
+        public string ICAOHex { get; set; }
+        public string FlightModel { get; set; }
+        public string EngineType { get; set; }
+        public int SeatCount { get; set; }
+        public DateTimeOffset DeliveryDate { get; set; }
+        public int PlaneAge { get; set; }
+        public string ImageURL { get; set; }
+
+        public Aircraft(string iCAOHex, string flightModel, string engineType, int seatCount, DateTimeOffset deliveryDate, int planeAge, string imageURL)
+        {
+            ICAOHex = iCAOHex;
+            FlightModel = flightModel;
+            EngineType = engineType;
+            SeatCount = seatCount;
+            DeliveryDate = deliveryDate;
+            PlaneAge = planeAge;
+            ImageURL = imageURL;
+        }
+    }
+}
